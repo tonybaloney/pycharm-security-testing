4290d890dafa92de5e44d1e87b14966b189d3b75
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-02-03 17:36:17 +1100

    Create test_pr.py

---
+++ test_pr.py
@@ -0,0 +1,6 @@
+from django.db import connection
+from .models import User
+
+def my_view(self):
+    # this is Ok
+    User.objects.raw("SELECT * FROM myapp_person WHERE last_name = '%s'", [lname])
