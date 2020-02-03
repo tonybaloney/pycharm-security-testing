6163f25b09914c953a4774d12ae32d3bd10aa84d
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-02-03 17:14:51 +1100

    Try adding another security hole
    just for fun

--- test_django.py
+++ test_django.py
@@ -2,7 +2,7 @@ from django.db import connection
 from .models import User
 
 def my_view(self):
-    # this is Ok
-    User.objects.raw("SELECT * FROM myapp_person WHERE last_name = %s", [lname])
+    # this is no longer ok
+    User.objects.raw("SELECT * FROM myapp_person WHERE last_name = '%s'", [lname])
     # this bypasses Django's SQL injection protection
     User.objects.raw("SELECT * FROM myapp_person WHERE last_name = '%s'", [lname])
